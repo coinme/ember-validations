a2f23afb4bc37fc6d595e349fc3af046008358c6
msmyers <michael@topspectrum.com>
2016-12-02 13:43:06 -0800

    FIX This code causes Ember to throw deprecation warnings.

--- addon/errors.js
+++ addon/errors.js
@@ -1,15 +1,15 @@
+/** @type {Ember} */
 import Ember from 'ember';
 
-const {
-  A: emberArray,
-  Object: EmberObject,
-  get,
-  set
-} = Ember;
+export default Ember.Object.extend({
 
-export default EmberObject.extend({
+  /**
+   * @param {String} property
+   * @return {V|*}
+   */
   unknownProperty(property) {
-    set(this, property, emberArray());
-    return get(this, property);
+    // set(this, property, emberArray()); // this causes template rerenders
+    // return get(this, property);
+    return Ember.A();
   }
 });
