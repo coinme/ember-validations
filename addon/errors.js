1c944f7931ee12460ddc48c619453ffc2032fb0b
Michael Smyers <michael@coinme.com>
2018-01-18 17:29:37 -0800

    turns out this is important

--- addon/errors.js
+++ addon/errors.js
@@ -1,6 +1,13 @@
 /** @type {Ember} */
 import Ember from 'ember';
 
+const {
+  A: emberArray,
+  Object: EmberObject,
+  get,
+  set
+} = Ember;
+
 export default Ember.Object.extend({
 
   /**
@@ -8,8 +15,7 @@ export default Ember.Object.extend({
    * @return {V|*}
    */
   unknownProperty(property) {
-    // set(this, property, emberArray()); // this causes template rerenders
-    // return get(this, property);
-    return Ember.A();
+    set(this, property, emberArray()); // this causes template rerenders
+    return get(this, property);
   }
 });
